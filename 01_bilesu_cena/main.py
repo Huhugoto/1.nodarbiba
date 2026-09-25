5128a8aa9bd0e8d860d168072ec4ace6d2aa1306
Huhugoto <henrijs.hugo@gmail.com>
2026-09-25 11:52:07 +0300

    bilesu_cena

--- 01_bilesu_cena/main.py
+++ 01_bilesu_cena/main.py
@@ -3,7 +3,20 @@ print("Biļešu cenas kalkulators")
 vecums = int(input("Ievadi savu vecumu: "))
 
 # TODO: ar if, elif un else nosaki pareizo cenu
-cena = 0
+if vecums <= 6:
+    cena = 0
+    print(f"Biļetes cena ir {cena} EUR.")
+if vecums >= 7 and vecums <= 17:
+    cena = 3
+    print(f"Biļetes cena ir {cena} EUR.")
+if vecums >= 18 and vecums <= 64:
+    cena = 7
+    print(f"Biļetes cena ir {cena} EUR.")
+if vecums >= 65:
+    cena = 4
+    print(f"Biļetes cena ir {cena} EUR.")
 
-print(f"Biļetes cena ir {cena} EUR.")
+
+
+
 

2539804fd6a069abb687ac9b3af354c5826c37ad
Huhugoto <henrijs.hugo@gmail.com>
2026-09-25 12:29:45 +0300

    ir punkti

--- 03_mini_viktorina/main.py
+++ 03_mini_viktorina/main.py
@@ -12,6 +12,7 @@ if atbilde == "print":
     punkti += 1
 atbilde = input("3. Kāds cikls iet cauri elementiem? ").lower()
 # TODO: pārbaudi trešo atbildi un palielini punktu skaitu
-
+if atbilde == "for":
+    punkti += 1
 print(f"Tu ieguvi {punkti} no 3 punktiem.")
 
